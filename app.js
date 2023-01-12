7168f460f968fef7d2b88e41761c637c4ae0c37b
tevisar <83377006+tevis-zorg@users.noreply.github.com>
2023-01-12 13:34:42 +0700

    Update app.js
    adding more logic to interact with mongoDB atlas. in this update, i try to interact with posting a user input into a database.

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const _ = require('ejs')
-// const port = require ('./server')
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blogs_data = require('./models/blogsDB');
+const _ = require('ejs');
 
 // Express app
 const app = express();
+
+// Connect into mongoDB
+const dbURI = 'mongodb+srv://<project_name>:<password>@ironin.z5tsj0q.mongodb.net/<cluster_name>?retryWrites=true&w=majority';
+mongoose.set('strictQuery' , false);//to supress the deprecation warning.
+mongoose.connect(dbURI , {useNewUrlParser : true, useUnifiedTopology : true})
+    .then((result) => app.listen(8080))
+    .catch((err) => console.log(err));
+
 
 // register view engine
 app.set('views' , './views_app');
@@ -14,26 +24,27 @@ app.use(express.static('./views_app/public'));//allowing css file to be displaye
 
 
 // Listen for request
-app.listen(8080);
+// app.listen(8080);
+
+// middleware 3rd parties & static files.
+app.use(express.static('./views_app/public'));//allowing css file to be displayed from head.ejs
+app.use(express.urlencoded({ extended : true }));//app.post middleware
+app.use(morgan('dev'));
 
 app.get('/' , (req, res) => {
     
-    const blogs = [
-        {title : "Yoshimitsuru Finds Cabbage" , snippet : "How do he get it anyway?"},
-        {title : "jinajinjin Discovering Wonder", snippet : "Did he never Wandering?"},
-        {title : "Howrangi Lost in utan" , snippet : "What? inside that ape?"}
-    ]
+    res.redirect('/blogs')
     
+})
+
+app.get('/about', (req, res) => {
     res.render(
-        'index', 
+        'about', 
         {
-            page_title : "I-Ronin Home", 
-            headings_title : "Ronin Blogs",
-            blogs,
-            
+            page_title : "I-Ronin About",
+            headings_title : "About I-Ronin page"
         }
     );
-    
 })
 
 app.get('/blogs/create' , (req, res) => {
@@ -48,23 +59,67 @@ app.get('/blogs/create' , (req, res) => {
     
 })
 
-app.get('/about', (req, res) => {
-    res.render(
-        'about', 
+//blog routes
+app.get('/blogs', (req, res) => {
+    Blogs_data.find().sort({createdAt:-1})
+    .then((result) => {
+        res.render('index' , 
         {
-            page_title : "I-Ronin About",
-            headings_title : "About I-Ronin page"
-        }
-    );
+         ronin_blogs: result,
+         page_title: 'Home' ,
+         headings_title: 'Ronin Blogs'
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 })
 
-// app.post((req, res) => {
-//     res.render('head', express.static(__dirname + './views_app/partial_views_app/styling.css'))
-// }) //try to figure out inputing css file
+//post blog handler
+app.post('/blogs' , (req, res) => {
+    // console.log(req.body);
+    const ronin_blogs = new Blogs_data(req.body);
+    
+    ronin_blogs.save()
+    .then((result) => {
+        res.redirect('/blogs');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
 
-// app.get('/about-us' , (req, res) => {
-//     res.status(304).redirect('/about');
-// })// testing purpose
+//routes parameter
+app.get('/blogs/:id', (req, res) => {
+    const identity = req.params.id
+    console.log(identity);
+    Blogs_data.findById(identity)
+    .then((result) => {
+        res.render(
+            'blog_detail' , 
+            {
+                ronin_blogs : result,
+                page_title : "A Ronin's Blog",
+                headings_title : `Ronin's blog`
+            }
+        );
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+})
+
+app.get('/blogs/create' , (req, res) => {
+    
+    res.render(
+        'create', 
+        {
+            page_title : "I-Ronin Create New Blog",
+            headings_title : "Create New I-Ronin Blog"
+        }
+    )
+    
+})
 
 app.use((req,res) => {
     res.status(404).render('404', {page_title : "Error:404"});
